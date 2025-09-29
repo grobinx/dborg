121d88e43ea16762ff2ea96a51a581f90eabd231
Andrzej Kałuża <akaluza@kamsoft.pl>
2025-09-29 13:13:52 +0200

    fix: refactor AppContainers component for improved readability and structure

--- src/renderer/src/app/AppContainers.tsx
+++ src/renderer/src/app/AppContainers.tsx
@@ -5,33 +5,31 @@ const AppContainers: React.FC = () => {
     const { containers, selectedContainer, selectedView } = useContainers();
 
     return (
-        <>
-            {containers?.map((container) => {
-                if (["new-connection", "connection-list", "connections"].includes(container.type)) {
-                    return (
-                        <Container key={container.type} hidden={container !== selectedContainer}>
-                            {container.container !== undefined && <container.container key={container.id} />}
-                        </Container>
-                    );
-                }
-                if (container !== selectedContainer) {
-                    return null; // Skip rendering if the container is not selected
-                }
+        containers?.map((container) => {
+            if (["new-connection", "connection-list", "connections"].includes(container.type)) {
                 return (
-                    <Container key={container.type}>
-                        {container.container !== undefined &&
-                            <container.container key={container.id} >
-                                {
-                                    selectedView?.type === "rendered" &&
-                                    selectedView?.render &&
-                                    <selectedView.render key={selectedView.id} />
-                                }
-                            </container.container>
-                        }
+                    <Container key={container.type} hidden={container !== selectedContainer}>
+                        {container.container !== undefined && <container.container key={container.id} />}
                     </Container>
                 );
-            })}
-        </>
+            }
+            if (container !== selectedContainer) {
+                return null; // Skip rendering if the container is not selected
+            }
+            return (
+                <Container key={container.type}>
+                    {container.container !== undefined &&
+                        <container.container key={container.id} >
+                            {
+                                selectedView?.type === "rendered" &&
+                                selectedView?.render &&
+                                <selectedView.render key={selectedView.id} />
+                            }
+                        </container.container>
+                    }
+                </Container>
+            );
+        })
     );
 };
 
