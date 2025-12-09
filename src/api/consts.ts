d7b6d3a899fdd0cf16fc6628dba274b27c453717
Andrzej Kałuża <akaluza@kamsoft.pl>
2025-12-09 12:51:48 +0100

    chore(package): update build script to include update-build step and increment build number

--- src/api/consts.ts
+++ src/api/consts.ts
@@ -5,7 +5,7 @@ export const version: Version = {
     major: 1,
     minor: 0,
     release: 5,
-    build: 401,
+    build: 402,
 
     toString: function () {
         return `${this.major}.${this.minor}.${this.release}.${this.build}`;
@@ -16,6 +16,6 @@ export const version: Version = {
 //export const dborgReleaseName: string = "Schrodinger's Sprout";
 export const dborgReleaseName: string = "Louver Gate";
 export const dborgDuration: string = "2024-2025";
-export const dborgDate: string = "2025-11-21 07:32:54";
+export const dborgDate: string = "2025-12-09 12:32:26";
 
 export const DBORG_DATABASE_DRIVER = "sqlite3";
